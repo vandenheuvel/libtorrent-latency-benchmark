5084d09024976fbb6ce6d8c9b5242cbb89ebe719
Bram van den Heuvel <b.vandenheuvel@student.tudelft.nl>
2016-09-23 19:51:26 +0200

    Changed printing for pytohn3

--- client.py
+++ client.py
@@ -4,10 +4,12 @@
 import libtorrent as lt
 import time
 
+torrent = open("test.torrent", 'rb')
+
 ses = lt.session()
 ses.listen_on(6881, 6891)
 
-e = lt.bdecode(open("test.torrent", 'rb').read())
+e = lt.bdecode(torrent.read())
 info = lt.torrent_info(e)
 
 params = { 'save_path': '.', 'storage_mode': lt.storage_mode_t.storage_mode_sparse, 'ti': info }
@@ -18,6 +20,6 @@ while (not s.is_seeding):
     s = h.status()
 
     state_str = ['queued', 'checking', 'downloading metadata', 'downloading', 'finished', 'seeding', 'allocating']
-    print '%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' %  (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, state_str[s.state])
+    print('%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' %  (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, state_str[s.state]))
 
     time.sleep(1)

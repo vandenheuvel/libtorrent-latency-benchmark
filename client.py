cb1fa8caef27670f39ffa8e2d56fcdcc1523eb2f
Bram van den Heuvel <b.vandenheuvel@student.tudelft.nl>
2016-09-23 19:19:41 +0200

    Add simple torrent client

---
+++ client.py
@@ -0,0 +1,23 @@
+# Retrieved on September 23st, 2016 from http://libtorrent.org/python_binding.html
+# Arvid Norberg is listed as the original author of the article.
+
+import libtorrent as lt
+import time
+
+ses = lt.session()
+ses.listen_on(6881, 6891)
+
+e = lt.bdecode(open("test.torrent", 'rb').read())
+info = lt.torrent_info(e)
+
+params = { 'save_path': '.', 'storage_mode': lt.storage_mode_t.storage_mode_sparse, 'ti': info }
+h = ses.add_torrent(params)
+
+s = h.status()
+while (not s.is_seeding):
+    s = h.status()
+
+    state_str = ['queued', 'checking', 'downloading metadata', 'downloading', 'finished', 'seeding', 'allocating']
+    print '%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' %  (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, state_str[s.state])
+
+    time.sleep(1)

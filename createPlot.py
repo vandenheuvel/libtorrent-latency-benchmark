7efa80101ddae1625ef584d53a53a772135683c7
Bram van den Heuvel <b.vandenheuvel@student.tudelft.nl>
2016-10-19 17:42:22 +0200

    Add comments

--- createPlot.py
+++ createPlot.py
@@ -7,10 +7,12 @@ numVars = 3
 if len(sys.argv) != numVars + 1:
     print("Give me", numVars, "arguments!")
 
+# Get the given arguments
 fileName = sys.argv[1]
 testDuration = int(sys.argv[2])
 latencyIntervals = int(sys.argv[3])
 
+# Read download speeds from the file
 data = np.genfromtxt(fileName, delimiter=',')
 delays = [latencyIntervals * step for step in range(len(data))]
 lines = plt.plot(np.linspace(0, testDuration, len(data)), data)

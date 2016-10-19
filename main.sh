7010d6f672c01df5e4b2f3bbb508d76a682ffbd3
Bram van den Heuvel <b.vandenheuvel@student.tudelft.nl>
2016-10-19 21:03:11 +0200

    Include downloading of dependencies

--- main.sh
+++ main.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
-BRIDGENAME="lxcbr0"
-MAINSCRIPT="main.py"
-NUMLEECHERS=1
-NUMSEEDERS=1
-FILESIZE=32
-DATAFILE="result.csv"
-RUNDURATION=30
-LATENCYINTERVALS=20
+# main.sh
+
+WORKINGDIR="/mnt"
 TMPFOLDER="tmp/"
 SEEDFOLDER=$TMPFOLDER"seeder/"
 LEECHFOLDER=$TMPFOLDER"leecher/"
+
+BRIDGENAME="lxcbr0"
+MAINSCRIPT="main.py"
+DATAFILE="result.csv"
+
 FILENAME="test.file"
 TORRENTNAME="test.torrent"
+
+NUMLEECHERS=1
+NUMSEEDERS=1
+FILESIZE=32
+RUNDURATION=30
+LATENCYINTERVALS=20
 
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root."
@@ -21,18 +27,18 @@ fi
 echo "Setting DNS..."
 echo -e "nameserver 8.8.8.8\nnameserver 208.67.222.222\n" > /etc/resolv.conf
 
+echo "Setting PATH variable..."
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 export PATH
 
 echo "Downloading dependencies..."
 DEPENDENCIES=("bridge-utils" "ctorrent" "python3-numpy" "python3-matplotlib" "lxc")
-
 for package in "${DEPENDENCIES[@]}"
 do
     apt install $package -y -qq
 done
 
-echo "Removing bridge with name $BRIDGENAME if one exists..."
+echo "Removing bridge with name $BRIDGENAME regardless of whether one exists..."
 ifconfig $BRIDGENAME down
 brctl delbr $BRIDGENAME
 
@@ -40,9 +46,8 @@ echo "Creating and setting up bridge $BRIDGENAME..."
 brctl addbr $BRIDGENAME
 ifconfig $BRIDGENAME up
 
-
-echo "Creating temporary folder to conduct tests in..."
-cd /mnt
+echo "Working from $WORKINGDIR. Creating temporary folder to conduct tests in..."
+cd $WORKINGDIR
 mkdir $TMPFOLDER
 mkdir $SEEDFOLDER
 mkdir $LEECHFOLDER
@@ -50,14 +55,20 @@ mkdir $LEECHFOLDER
 echo "Copying leecher and seeder scripts to the correct folders..."
 cp seeder.py $SEEDFOLDER 
 cp leecher.py $LEECHFOLDER
+cp dependencies.sh $SEEDFOLDER
+cp dependencies.sh $LEECHFOLDER
 
 echo "Creating random file of $FILESIZE MB and torrent for the seeders to seed... This might take a while."
 dd if=/dev/urandom of=$SEEDFOLDER$FILENAME bs=1M count=$FILESIZE status=progress
 ctorrent -t -u 127.0.0.1 -s $SEEDFOLDER$TORRENTNAME $SEEDFOLDER$FILENAME
 cp $SEEDFOLDER$TORRENTNAME $LEECHFOLDER$TORRENTNAME
 
-echo "Running Python script $MAINSCRIPT with parameters $BRIDGENAME $NUMLEECHERS $NUMSEEDERS..."
+echo "Downloading dependencies for seeders and leechers..."
+./dependencies.sh -d
+
+echo "\n---\n---\nRunning Python script $MAINSCRIPT with parameters $BRIDGENAME $NUMLEECHERS $NUMSEEDERS...\n\n"
 python3 $MAINSCRIPT $BRIDGENAME $NUMLEECHERS $NUMSEEDERS
+echo "\n\nDone running Python script $MAINSCRIPT.\n---\n---\n"
 
 echo "Copying results..."
 mv $LEECHFOLDER$DATAFILE .

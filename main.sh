017e65235c22743441848a0aa11ec869e64ee82a
Bram van den Heuvel <b.vandenheuvel@student.tudelft.nl>
2016-10-19 20:54:04 +0200

    Add comment in main

--- main.sh
+++ main.sh
@@ -51,7 +51,7 @@ echo "Copying leecher and seeder scripts to the correct folders..."
 cp seeder.py $SEEDFOLDER 
 cp leecher.py $LEECHFOLDER
 
-echo "Creating random file and torrent for the seeders to seed..."
+echo "Creating random file of $FILESIZE MB and torrent for the seeders to seed... This might take a while."
 dd if=/dev/urandom of=$SEEDFOLDER$FILENAME bs=1M count=$FILESIZE status=progress
 ctorrent -t -u 127.0.0.1 -s $SEEDFOLDER$TORRENTNAME $SEEDFOLDER$FILENAME
 cp $SEEDFOLDER$TORRENTNAME $LEECHFOLDER$TORRENTNAME

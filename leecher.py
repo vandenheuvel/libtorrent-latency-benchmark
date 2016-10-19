256bd8daffb72ea5d221b354cae5719be937e932
Max van Deursen <M.vanDeursen@student.tudelft.nl>
2016-10-19 17:57:35 +0200

    Add comments to leecher.py

--- leecher.py
+++ leecher.py
@@ -1,35 +1,50 @@
-# Retrieved on September 23st, 2016 from http://libtorrent.org/python_binding.html
-# Arvid Norberg is listed as the original author of the article.
-
+#!/usr/bin/env python3
+# leecher.py
+ 
+"""This script tests the downloading speed from the given IPS of different latencies.
+This is done by netem to emulate the latency.
+The results are then saved to a .csv file"""
 import os
 import sys
 import time
 import numpy
 import libtorrent as lt
-import matplotlib.pyplot as plt
 
-latencies = [50 * x for x in range(10)]
+# Give the starting IP and the number of IPs
+numVars = 2
+if len(sys.argv) != numVars + 1:
+    print("This script requires two arguments, the starting IP and the number of IPs respectively.")
 
-downloadFolder = 'downloads/'
-torrentFolder = 'torrents/'
+startIP = int(sys.argv[1])
+numIPs = int(sys.argv[2])
+
+# Creation of variables used within the function
+downloadFolder = './'
+torrentFolder = './'
 torrentName = 'test.torrent'
-figureName = 'figure'
-
-networkDevice = 'enp0s8'
-
+fileName = "test.file"
+networkDevice = 'eth0'
 measureEvery = .01
+latencyInterval = 50
+numIntervals = 10
 totalTime = 30
-
 iterations = round(totalTime / measureEvery)
+
+# Creation of both the latencies to be used and the save array for the speeds
+latencies = [latencyInterval * x for x in range(numIntervals)]
 speeds = [[0 for x in range(iterations)] for y in latencies]
 
-os.system('rm ' + downloadFolder + 'torrentTest1GB')
+# Remove prior settings and downloaded torrents
+os.system('rm ' + downloadFolder + fileName)
 os.system('sudo tc qdisc del dev ' + networkDevice + ' root netem')
+
 for index, latency in enumerate(latencies):
-    torrent = open(torrentFolder + torrentName, 'rb')
     print('\nNow testing with latency', latency, '...')
+    # Adding latency to the network device
     os.system('sudo tc qdisc add dev ' + networkDevice + ' root netem delay ' + str(latency) + 'ms')
 
+    # Open the torrent and start downloading
+    torrent = open(torrentFolder + torrentName, 'rb')
     ses = lt.session()
     ses.listen_on(6881, 6891)
 
@@ -39,9 +54,11 @@ for index, latency in enumerate(latencies):
     params = { 'save_path': downloadFolder, 'storage_mode': lt.storage_mode_t.storage_mode_sparse, 'ti': info }
     h = ses.add_torrent(params)
     
-    for ipAddress in range(int(sys.argv[1]), (int(sys.argv[1]) + int(sys.argv[2]) + 1))
+    # Add the peers to the torrent
+    for ipAddress in range(startIP, (startIP + numIPs + 1)):
         h.connect_peer(('192.168.1.' + str(ipAddress), 6881), 0x01)
 
+    # Save data for the amount of iterations into speed
     for i in range(iterations):
         sys.stdout.write('\r%.2f%%' % (100 * i / iterations))
         s = h.status()
@@ -52,8 +69,10 @@ for index, latency in enumerate(latencies):
         if s.is_seeding:
             break
 
+    # Remove latency settings and the downloaded torrent.
     os.system('sudo tc qdisc del dev ' + networkDevice + ' root netem')
-    os.system('rm ' + downloadFolder + 'torrentTest1GB')
+    os.system('rm ' + downloadFolder + fileName)
 
+# Write the speeds array to a .csv file
 npSpeeds = numpy.array(speeds).transpose()
 numpy.savetxt("speeds.csv", npSpeeds, delimiter = ",")

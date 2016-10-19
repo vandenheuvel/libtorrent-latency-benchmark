d234faeb2925f5d5ca07e4ece8bf3de23bec8dda
Bram van den Heuvel <b.vandenheuvel@student.tudelft.nl>
2016-10-19 17:47:56 +0200

    Add comments

--- seeder.py
+++ seeder.py
@@ -1,20 +1,28 @@
-# Initial version retrieved on September 23st, 2016 from http://libtorrent.org/python_binding.html
-# Arvid Norberg is listed as the original author of the article.
+#!/usr/bin/env python3 
+# seeder.py
+
+"""This script seeds the specified torrent file."""
 
 import libtorrent as lt
 
+# Choose from which directory to seed, and where the torrent is stored
 downloadFolder = './'
 torrentFolder = './'
 torrentName = 'test.torrent'
 
+# Read the torrent file
 torrent = open(torrentFolder + torrentName, 'rb')
 
+# Start a libtorrent session
 ses = lt.session()
+
+# Listen on default port range
 ses.listen_on(6881, 6891)
 
 e = lt.bdecode(torrent.read())
 info = lt.torrent_info(e)
 
+# Add the torrent and start seeding
 params = { 'save_path': downloadFolder, 'storage_mode': lt.storage_mode_t.storage_mode_sparse, 'ti': info }
 h = ses.add_torrent(params)
 
